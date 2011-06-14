dd8e03dba9b2bac4f660185cfb01b3234dba50bb
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2011-06-14 14:34:15 -0600

    refactoring models

--- trocaire/ingresos/models.py
+++ trocaire/ingresos/models.py
@@ -24,32 +24,38 @@ choice_clasificacion = (
 class Fuentes(models.Model):
     nombre = models.CharField(max_length=200)
     clasificacion = models.IntegerField(choices=choice_clasificacion)
+
     def __unicode__(self):
         return self.nombre
         
 class PrincipalesFuentes(models.Model):
     fuente = models.ManyToManyField(Fuentes, verbose_name="Fuentes de ingreso")
     encuesta = models.ForeignKey(Encuesta)
+
     class Meta:
         verbose_name_plural = "Cuales son las principales fuentes de ingreso de la familia"
         
 class CIPeriodos(models.Model):
     nombre = models.CharField(max_length=200)
+
     def __unicode__(self):
         return self.nombre
         
 class CIPermanentes(models.Model):
     nombre = models.CharField(max_length=200)
+
     def __unicode__(self):
         return self.nombre
         
 class CIEstacionales(models.Model):
     nombre = models.CharField(max_length=200)
+
     def __unicode__(self):
         return self.nombre
         
 class CIHortalizas(models.Model):
     nombre = models.CharField(max_length=200)
+
     def __unicode__(self):
         return self.nombre
         
@@ -62,18 +68,21 @@ class CultivosIPeriodos(models.Model):
     precio_postrera = models.FloatField('Precio de venta de ciclo de postrera', help_text="En C$")
     precio_apante = models.FloatField('Precio de venta de ciclo de apante', help_text="En C$")
     encuesta = models.ForeignKey(Encuesta)
+
     class Meta:
         verbose_name_plural = "Ventas agricolas"
         
-    def suma_primera(self):
-        primera = self.cuanto_primera * self.precio_primera
-        return primera
-    def suma_postrera(self):
-        postrera = self.cuanto_postrera * self.precio_postrera
-        return postrera
-    def suma_apante(self):
-        apante = self.cuanto_apante * self.precio_apante
-        return apante
+    def total_primera(self):
+        return self.cuanto_primera * self.precio_primera
+
+    def total_postrera(self):
+        return self.cuanto_postrera * self.precio_postrera
+
+    def total_apante(self):
+        return self.cuanto_apante * self.precio_apante
+    
+    def total(self):
+        return self.total_primera() + self.total_postrera() + self.total_apante()
         
 class CultivosIPermanentes(models.Model):
     cultivo = models.ForeignKey(CIPermanentes)
@@ -83,7 +92,7 @@ class CultivosIPermanentes(models.Model):
     class Meta:
         verbose_name_plural = "Cultivos Permanentes"
         
-    def perma(self):
+    def total(self):
         total = self.cuanto * self.precio
         
         return total
@@ -96,14 +105,8 @@ class CultivosIEstacionales(models.Model):
     class Meta:
         verbose_name_plural = "Cultivos Estacionales"
         
-#    def save(self, *args, **kwargs)
-#        self.total = self.cuanto * self.precio
-#        super(CultivosIEstacionales, self).save(*args, **kwargs)
-        
-    def estacional(self):
-        total = self.cuanto * self.precio
-        
-        return total
+    def total(self):
+        return self.cuanto * self.precio
 
 class IHortalizas(models.Model):
     hortaliza = models.ForeignKey(CIHortalizas)
@@ -114,10 +117,8 @@ class IHortalizas(models.Model):
     class Meta:
         verbose_name_plural = "Hortalizas"
         
-    def hortalizas(self):
-        total = self.cuanto * self.precio
-        
-        return total
+    def total(self):
+        return tself.cuanto * self.precio
         
 class IngresoPatio(models.Model):
     invierno = models.FloatField('Monto de los ingreso obtenido en invierno')
@@ -126,16 +127,14 @@ class IngresoPatio(models.Model):
     class Meta:
         verbose_name_plural = "Ingreso por la producción agrícola del patio"
         
-    def sumar(*args):
-        pass
-        
-    def patio(self):
+    def total(self):
         total = self.invierno + self.verano
         
         return total
         
 class Ganados(models.Model):
     nombre = models.CharField(max_length=200)
+
     def __unicode__(self):
         return self.nombre
 
@@ -144,17 +143,17 @@ class IngresoGanado(models.Model):
     vendidos = models.IntegerField('Número de animales vendidos')
     valor = models.FloatField('Valor de venta')
     encuesta = models.ForeignKey(Encuesta)
+
     class Meta:
         verbose_name_plural = "Ingresos por la comercialización del ganado mayor y menor"
         
-    def ganados(self):
-        total = self.vendidos * self.valor
-        
-        return total
+    def total(self):
+        return self.vendidos * self.valor
         
 class Productos(models.Model):
     nombre = models.CharField(max_length=200)
     unidad = models.CharField(max_length=20)
+
     def __unicode__(self):
         return self.nombre
                
@@ -165,17 +164,16 @@ class Lactios(models.Model):
     verano_precio = models.FloatField()
     cantidad_vera = models.FloatField('Cantidad en Verano')
     encuesta = models.ForeignKey(Encuesta)
+
     class Meta:
         verbose_name_plural = "Producto en el ultimo año"
         
-    def lacti(self):
-        lista = []
+    def total(self):
+        #TODO: REVISAR
         invierno = self.cantidad_invi * self.invierno_precio
         verano = self.cantidad_vera * self.verano_precio
         total = invierno + verano
-        
-        lista = [invierno, verano, total]
-        return lista
+        return [invierno, verano, total]
         
 class PProcesado(models.Model):
     nombre = models.CharField(max_length=200)
@@ -187,16 +185,16 @@ class ProductosProcesado(models.Model):
     cantidad = models.FloatField('Cantidad vendida anualmente')
     monto = models.FloatField('Monto de los ingresos obtenidos')
     encuesta = models.ForeignKey(Encuesta)
+
     class Meta:
         verbose_name_plural = "Ingresos por la producción, comercializacion e ingresos por productos procesados"
         
-    def procesado(self):
-        total = self.monto
-        
-        return total        
+    def total(self):
+        return self.monto 
         
 class OtrasActividades(models.Model):
     nombre = models.CharField(max_length=200)
+
     def __unicode__(self):
         return self.nombre
         
@@ -215,12 +213,12 @@ class OtrosIngresos(models.Model):
     marzo = models.FloatField()
     abril = models.FloatField()
     encuesta = models.ForeignKey(Encuesta)
+
     class Meta:
         verbose_name_plural = "Otros ingresos en el nucleo familiar (Estimación de ingresos anuales)"
         
-    def otros(self):
-        total = self.mayo + self.junio + self.julio + self.agosto + self.septiembre + self.octubre + self.noviembre + self.diciembre + self.enero + self.febrero + self.marzo + self.abril
-        return total
+    def total(self):
+        return  self.mayo + self.junio + self.julio + self.agosto + self.septiembre + self.octubre + self.noviembre + self.diciembre + self.enero + self.febrero + self.marzo + self.abril
         
 choice_vendes = (
                                 (1,'1. Vende Individual. No incluye vender en ferias'),
@@ -235,11 +233,13 @@ class PrincipalForma(models.Model):
     principal = models.IntegerField('121. Cuál es la principal forma de comercializar su producción',
                                     choices=choice_vendes)
     encuesta = models.ForeignKey(Encuesta)
+
     class Meta:
         verbose_name_plural = "Principal Forma de comercializar su produccion"
         
 class ProductosPrincipales(models.Model):
     nombre = models.CharField(max_length=200)
+
     def __unicode__(self):
         return self.nombre
         
@@ -247,5 +247,6 @@ class VendeProducto(models.Model):
     principal = models.ForeignKey(ProductosPrincipales, verbose_name="Rubros")
     forma = models.IntegerField('Forma principal de venta', choices=choice_vendes)
     encuesta = models.ForeignKey(Encuesta)
+
     class Meta:
         verbose_name_plural = "122. Como vende cada uno de los siguientes productos"  
