d903f6a9a6d5dd52d0e169df6bf592099ca121ff
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2011-07-28 15:50:57 -0600

    componiendo indicadores generales

--- trocaire/ingresos/urls.py
+++ trocaire/ingresos/urls.py
@@ -8,9 +8,9 @@ urlpatterns = patterns('trocaire.ingresos.views',
     url(r'rangos/(?P<maximo>\d+)/(?P<minimo>\d+)/(?P<separaciones>\d+)/$', 'generic_range', {'model':'totalingreso', 'field': 'total'}, name="ingresos_generic_range"),
     url(r'rangos/(?P<maximo>\d+)/(?P<minimo>\d+)/(?P<separaciones>\d+)/$', 'generic_range', {'model':'totalingreso', 'field': 'total'}, name="ingresos_generic_range"),
     #ingreso ganado
-    url(r'rangos/ganado/$', 'generic_range', {'model': 'ingresoganado', 'field': 'total'}, name="ingresos_generic_range"),
-    url(r'rangos/ganado/(?P<maximo>\d+)/(?P<minimo>\d+)/(?P<separaciones>\d+)/$', 'generic_range', {'model': 'ingresoganado', 'field': 'total'}, name="ingresos_generic_range"),
-    url(r'rangos/ganado/(?P<maximo>\d+)/(?P<minimo>\d+)/(?P<separaciones>\d+)/$', 'generic_range', {'model': 'ingresoganado', 'field': 'total'}, name="ingresos_generic_range"),
+    url(r'rangos/ganado/$', 'generic_range', {'model': 'ingresoganado', 'field': 'total'}, name="ingresos_ganado"),
+    url(r'rangos/ganado/(?P<maximo>\d+)/(?P<minimo>\d+)/(?P<separaciones>\d+)/$', 'generic_range', {'model': 'ingresoganado', 'field': 'total'}, name="ingresos_ganado"),
+    url(r'rangos/ganado/(?P<maximo>\d+)/(?P<minimo>\d+)/(?P<separaciones>\d+)/$', 'generic_range', {'model': 'ingresoganado', 'field': 'total'}, name="ingresos_ganado"),
     #fuentes ap
     url(r'rangos/fuentes-ap/$', 'generic_range', {'model': 'principalesfuentes', 'field': 'fuentes_ap'}, name="fuentes_ap_generic_range"),
     url(r'rangos/fuentes-ap/(?P<maximo>\d+)/(?P<minimo>\d+)/(?P<separaciones>\d+)/$', 'generic_range', {'model': 'principalesfuentes', 'field': 'fuentes_ap'}, name="fuentes_ap_generic_range"),
