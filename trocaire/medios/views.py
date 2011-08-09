1de305594e0bf38ae3f54ae76b8dcd19aa7f07a3
Helmy Giacoman <helmygb@gmail.com>
2011-08-09 15:36:02 -0600

    fix 2 salidas sencillas

--- trocaire/medios/views.py
+++ trocaire/medios/views.py
@@ -58,9 +58,7 @@ def _query_set_filtrado(request):
             if len(request.session['parametros'][key]):
                 reducir = True if (last_key[1] != key > 1 and last_key[0] == None) or reducir==True else False
                 last_key = (i, key)
-                ids = model.objects.filter(**request.session['parametros'][key]).values_list('encuesta__id', flat=True)
-                print '---------------------------------------------'
-                print len(ids)                
+                ids = model.objects.filter(**request.session['parametros'][key]).values_list('encuesta__id', flat=True)                              
                 encuestas_id += ids
         if not encuestas_id:
             return Encuesta.objects.filter(**params)
@@ -211,17 +209,11 @@ def indicadores(request):
 def datos_sexo(request):    
     encuestas = _query_set_filtrado(request).values_list('id', flat=True)    
     composicion_familia = Composicion.objects.filter(encuesta__id__in=encuestas)
-    '''1: Hombre, 2: Mujer, 3: Compartido'''    
-    tabla_sexo_jefe = {1: 0, 2: 0, 3: 0}
+    '''1: Hombre, 2: Mujer'''    
+    tabla_sexo_jefe = {1: 0, 2: 0}
     tabla_sexo_beneficiario = {}
-    for composicion in composicion_familia:
-        #validar si el beneficiario es el jefe de familia
-        if composicion.beneficio == 1:
-            tabla_sexo_jefe[composicion.sexo] += 1
-        elif composicion.beneficio == 2:
-            tabla_sexo_jefe[composicion.sexo_jefe] += 1            
-        else:
-            tabla_sexo_jefe[3] += 1
+    tabla_sexo_jefe[1] = encuestas.filter(sexo_jefe=1).count()
+    tabla_sexo_jefe[2] = encuestas.filter(sexo_jefe=2).count()
     tabla_sexo_beneficiario['masculino'] = composicion_familia.filter(sexo=1).count()
     tabla_sexo_beneficiario['femenino'] = composicion_familia.filter(sexo=2).count()                
     dondetoy = "sexojefe"
@@ -452,25 +444,15 @@ def mujeres_decisiones(request):
     return render_to_response('encuestas/mujeres_decisiones.html', RequestContext(request,locals()))
     
 def sexo_beneficiario(request):
-    encuestas = _query_set_filtrado(request).values_list('id', flat=True)
-    composicion_familia = Composicion.objects.filter(encuesta__id__in=encuestas)
+    encuestas = _query_set_filtrado(request).values_list('id', flat=True)    
     '''1: hombre, 2: mujer'''
-    mujer_jefe = {1:0, 2:0}
-    hombre_jefe = {1:0, 2:0}
+       
+    query_hombre = Composicion.objects.filter(encuesta__id__in=encuestas.filter(sexo_jefe=1))
+    query_mujer = Composicion.objects.filter(encuesta__id__in=encuestas.filter(sexo_jefe=2))
     
-    for composicion in composicion_familia:
-        #validando si el beneficiario es el jefe y es hombre
-        if composicion.beneficio == 1 and composicion.sexo == 1:            
-            hombre_jefe[1] += 1
-        #si el beneficiario es jefe y es mujer
-        elif composicion.beneficio == 1 and composicion.sexo == 2:
-            mujer_jefe[2] += 1
-        #si el beneficiario no es el jefe, buscar su sexo
-        elif composicion.beneficio == 2:
-            if composicion.sexo_jefe == 1:
-                hombre_jefe[composicion.sexo] += 1
-            elif composicion.sexo_jefe == 2:
-                mujer_jefe[composicion.sexo] += 1                        
+    mujer_jefe = {1:query_mujer.filter(sexo=1).count(), 2:query_mujer.filter(sexo=2).count()}
+    hombre_jefe = {1:query_hombre.filter(sexo=1).count(), 2:query_hombre.filter(sexo=2).count()}
+                            
     dondetoy = "sexobene"
     return render_to_response('encuestas/sexo_beneficiario.html', RequestContext(request, locals()))
 
